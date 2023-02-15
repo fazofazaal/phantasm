104354ae740a31c6ed52f13c377b924ba31430e4
Hussain Fazaal <fazaalmv@gmail.com>
2023-02-15 23:32:36 +0500

    Created a new Plasmo extension

---
+++ contents/plasmo-root-container.tsx
@@ -0,0 +1,37 @@
+import type { PlasmoCSConfig } from "plasmo"
+import { createRoot } from "react-dom/client"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://www.plasmo.com/*"]
+}
+
+export const getRootContainer = () =>
+  new Promise((resolve) => {
+    const checkInterval = setInterval(() => {
+      const rootContainer = document.getElementById("itero")
+      if (rootContainer) {
+        clearInterval(checkInterval)
+        resolve(rootContainer)
+      }
+    }, 137)
+  })
+
+const PlasmoOverlay = () => {
+  return (
+    <span
+      style={{
+        background: "yellow",
+        padding: 12
+      }}>
+      HELLO WORLD ROOT CONTAINER
+    </span>
+  )
+}
+
+export const render = async ({ createRootContainer }) => {
+  const rootContainer = await createRootContainer()
+  const root = createRoot(rootContainer)
+  root.render(<PlasmoOverlay />)
+}
+
+export default PlasmoOverlay
