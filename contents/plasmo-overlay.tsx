c7e00c50d8a0af0517ccedddd6a6cab2ca8c3399
Hussain Fazaal <fazaalmv@gmail.com>
2023-02-16 20:29:16 +0500

    Added an experimental localhost overlay on plasmo.com wip

--- contents/plasmo-overlay.tsx
+++ contents/plasmo-overlay.tsx
@@ -2,7 +2,8 @@ import cssText from "data-text:~/contents/plasmo-overlay.css"
 import type { PlasmoCSConfig } from "plasmo"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.plasmo.com/*"],
+  matches: ["https://plasmo.com"],
+  all_frames: true,
   css: ["font.css"]
 }
 
@@ -14,13 +15,30 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   return (
-    <span
-      className="hw-top"
-      style={{
-        padding: 12
-      }}>
-      HELLO WORLD TOP
-    </span>
+    <>
+      {/* <span
+        className="hw-top"
+        style={{
+          padding: 12,
+          width: "100vw",
+          height: "100vh",
+          opacity: 0.5
+        }}>
+        HELLO WORLD TOP
+      </span> */}
+      <iframe
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        // width="300"
+        // height="200"
+        style={{
+          padding: 12,
+          width: "100vw",
+          height: "100vh",
+          opacity: 0.5
+        }}
+        src="http://localhost:3000/"></iframe>
+    </>
   )
 }
 
